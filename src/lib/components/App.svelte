a11744c00df86b2eacff9af9a066cce901c53c23
Floyd <floydtjones@gmail.com>
2024-02-04 23:04:15 -0500

    add maze and moving enemies

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,8 +1,54 @@
 <script lang="ts">
-  import { Canvas } from '@threlte/core'
-  import Scene from './Scene.svelte'
+	import { Canvas } from '@threlte/core';
+	import Scene from './Scene.svelte';
 </script>
 
+<div class="name">Floyd Jones</div>
+<div class="intro">Full Stack Developer</div>
+<div class="images">
+	<a target="__blank" href="https://github.com/flooyd">
+		<img src="github.png" alt="github" />
+	</a>
+	<a target="__blank" href="https://linkedin.com/in/flooyd">
+    <img src="linkedin.webp" alt="linkedin" />
+  </a>
+</div>
 <Canvas>
-  <Scene />
+	<Scene />
 </Canvas>
+
+<style>
+	* {
+		font-family: 'Roboto', sans-serif;
+	}
+
+	.name {
+		font-size: 42px;
+		font-weight: bold;
+		position: absolute;
+		top: 20px;
+		width: 100%;
+		text-align: center;
+	}
+
+	.intro {
+		font-size: 24px;
+		position: absolute;
+		top: 80px;
+		width: 100%;
+		text-align: center;
+    z-index: 2;
+	}
+
+	img {
+		width: 50px;
+		height: 50px;
+	}
+
+	.images {
+		position: absolute;
+		top: 120px;
+		width: 100%;
+		text-align: center;
+	}
+</style>
--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,81 +1,406 @@
 <script lang="ts">
-  import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+	import { T } from '@threlte/core';
+	import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras';
+	import { interactivity } from '@threlte/extras';
+	import { v4 as uuidv4 } from 'uuid';
+	import {gsap} from 'gsap';
+
+	interactivity();
+
+	type Wall = {
+		position: [number, number, number];
+		rotation: [number, number, number];
+		scale: [number, number, number];
+		color?: string | undefined;
+		id?: string | undefined;
+	};
+
+	type Enemy = {
+		position: [number, number, number];
+		rotation: [number, number, number];
+		scale: [number, number, number];
+		color?: string | undefined;
+		id?: string | undefined;
+		step: number;
+	};
+
+	const startingPosition = [1, .75, 1.5];
+
+	let steps = [
+		//start from startingPosition
+		[1, .75, 1.5],
+		//move up on X by .5
+		[1.5, .75, 1.5],
+		[2, .75, 1.5],
+		[2.5, .75, 1.5],
+		[3, .75, 1.5],
+		[3.5, .75, 1.5],
+		[4, .75, 1.5],
+		[4.5, .75, 1.5],
+		[5, .75, 1.5],
+		[5.5, .75, 1.5],
+		[6, .75, 1.5],
+		[6.5, .75, 1.5],
+		[7, .75, 1.5],
+		[7.5, .75, 1.5],
+		//move right on Z by .5
+		[7.5, .75, 2],
+		[7.5, .75, 2.5],
+		[7.5, .75, 3],
+		[7.5, .75, 3.5],
+		[7.5, .75, 4],
+		[7.5, .75, 4.5],
+		[7.5, .75, 5],
+		[7.5, .75, 5.5],
+		[7.5, .75, 6],
+		[7.5, .75, 6.5],
+		[7.5, .75, 7],
+		[7.5, .75, 7.5],
+		//move down on X
+		[7, .75, 7.5],
+		[6.5, .75, 7.5],
+		[6, .75, 7.5],
+		[5.5, .75, 7.5],
+		[5, .75, 7.5],
+		[4.5, .75, 7.5],
+		[4, .75, 7.5],
+		[3.5, .75, 7.5],
+		[3, .75, 7.5],
+		[2.5, .75, 7.5],
+		[2, .75, 7.5],
+		[1.5, .75, 7.5],
+		[1, .75, 7.5],
+		[.5, .75, 7.5],
+		[0, .75, 7.5],
+		[-.5, .75, 7.5],
+		[-1, .75, 7.5],
+		[-1.5, .75, 7.5],
+		[-2, .75, 7.5],
+		[-2.5, .75, 7.5],
+		[-3, .75, 7.5],
+		[-3.5, .75, 7.5],
+		[-4, .75, 7.5],
+		[-4.5, .75, 7.5],
+		[-5, .75, 7.5],
+		[-5.5, .75, 7.5],
+		[-6, .75, 7.5],
+		[-6.5, .75, 7.5],
+		[-7, .75, 7.5],
+		[-7.5, .75, 7.5],
+		//move left on Z
+		[-7.5, .75, 7],
+		[-7.5, .75, 6.5],
+		[-7.5, .75, 6],
+		[-7.5, .75, 5.5],
+		[-7.5, .75, 5],
+		[-7.5, .75, 4.5],
+		[-7.5, .75, 4],
+		[-7.5, .75, 3.5],
+		[-7.5, .75, 3],
+		[-7.5, .75, 2.5],
+		[-7.5, .75, 2],
+		[-7.5, .75, 1.5],
+		[-7.5, .75, 1],
+		[-7.5, .75, .5],
+		[-7.5, .75, 0],
+		[-7.5, .75, -.5],
+		[-7.5, .75, -1],
+		[-7.5, .75, -1.5],
+		[-7.5, .75, -2],
+		[-7.5, .75, -2.5],
+		[-7.5, .75, -3],
+		[-7.5, .75, -3.5],
+		[-7.5, .75, -4],
+		[-7.5, .75, -4.5],
+		[-7.5, .75, -5],
+		[-7.5, .75, -5.5],
+		[-7.5, .75, -6],
+		[-7.5, .75, -6.5],
+		[-7.5, .75, -7],
+		[-7.5, .75, -7.5],
+		//move up on X
+		[-7, .75, -7.5],
+		[-6.5, .75, -7.5],
+		[-6, .75, -7.5],
+		[-5.5, .75, -7.5],
+		[-5, .75, -7.5],
+		[-4.5, .75, -7.5],
+		[-4, .75, -7.5],
+		[-3.5, .75, -7.5],
+		[-3, .75, -7.5],
+		[-2.5, .75, -7.5],
+		[-2, .75, -7.5],
+		[-1.5, .75, -7.5],
+		[-1, .75, -7.5],
+		[-.5, .75, -7.5],
+		[0, .75, -7.5],
+		[.5, .75, -7.5],
+		[1, .75, -7.5],
+		[1.5, .75, -7.5],
+		[2, .75, -7.5],
+		[2.5, .75, -7.5],
+		[3, .75, -7.5],
+		[3.5, .75, -7.5],
+		[4, .75, -7.5],
+		[4.5, .75, -7.5],
+		[5, .75, -7.5],
+		[5.5, .75, -7.5],
+		[6, .75, -7.5],
+		[6.5, .75, -7.5],
+		[7, .75, -7.5],
+		[7.5, .75, -7.5],
+		[8, .75, -7.5],
+		[8.5, .75, -7.5],
+		[9, .75, -7.5],
+		[9.5, .75, -7.5],
+		[10, .75, -7.5],
+		[10.5, .75, -7.5],
+
+	]
+
+	let enemies = [
+		{
+			position: [1, .75, 1.5],
+			rotation: [0, 0, 0],
+			scale: [1, 1, 1],
+			color: 'red',
+			id: uuidv4(),
+			step: 0
+		}
+	];
+
+	const spawnEnemy = () => {
+		enemies.push({
+			position: [1, .75, 1.5],
+			rotation: [0, 0, 0],
+			scale: [1, 1, 1],
+			color: 'red',
+			id: uuidv4(),
+			step: 0
+		});
+		enemies = enemies;
+	}
+
+	const moveEnemies = () => {
+		enemies = enemies.map((enemy) => {
+			const nextStep = steps[enemy.step + 1];
+			if (nextStep) {
+				enemy.position = nextStep;
+				enemy.step = enemy.step + 1;
+			}
+			return enemy;
+		}).filter((enemy) => {
+			return steps[enemy.step + 1];
+		});
+		enemies = enemies;
+	}
+
+	let walls: Wall[] = [
+		/**
+  10 walls, then turn right and go 9 walls
+   */
+		{ position: [0, 0.5, 0], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [1, 0.5, 0], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [2, 0.5, 0], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [3, 0.5, 0], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [4, 0.5, 0], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [5, 0.5, 0], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [6, 0.5, 0], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [7, 0.5, 0], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [8, 0.5, 0], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [9, 0.5, 0], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [9, 0.5, 1], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [9, 0.5, 2], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [9, 0.5, 3], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [9, 0.5, 4], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [9, 0.5, 5], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [9, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [9, 0.5, 7], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [9, 0.5, 8], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [9, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		/**
+		 * turn right and go 18 walls
+		 */
+		{ position: [8, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [7, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [6, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [5, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [4, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [3, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [2, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [1, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [0, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-1, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-2, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-3, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-4, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-5, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-6, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-7, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-8, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, 9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		/**
+		 * then turn right and go 18 walls
+		 */
+		{ position: [-9, 0.5, 8], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, 7], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, 5], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, 4], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, 3], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, 2], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, 1], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, 0], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, -1], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, -2], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, -3], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, -4], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, -5], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, -7], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, -8], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-9, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		/**
+		 * then turn right and go 18 walls
+		 */
+		{ position: [-8, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-7, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-6, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-5, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-4, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-3, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-2, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-1, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [0, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [1, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [2, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [3, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [4, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [5, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [6, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [7, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [8, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [9, 0.5, -9], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		//start from the beginning of the maze, and go 3 walls to the right
+		{ position: [0, 0.5, 1], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [0, 0.5, 2], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [0, 0.5, 3], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		//go up 6 walls on X axis
+		{ position: [1, 0.5, 3], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [2, 0.5, 3], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [3, 0.5, 3], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [4, 0.5, 3], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [5, 0.5, 3], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [6, 0.5, 3], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		//go right 3 walls
+		{ position: [6, 0.5, 4], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [6, 0.5, 5], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [6, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		//go down 12 walls on X axis
+		{ position: [5, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [4, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [3, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [2, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [1, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [0, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-1, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-2, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-3, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-4, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-5, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-6, 0.5, 6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		//go left 12 walls
+		{ position: [-6, 0.5, 5], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-6, 0.5, 4], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-6, 0.5, 3], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-6, 0.5, 2], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-6, 0.5, 1], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-6, 0.5, 0], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-6, 0.5, -1], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-6, 0.5, -2], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-6, 0.5, -3], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-6, 0.5, -4], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-6, 0.5, -5], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-6, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		//go up 15 walls
+		{ position: [-5, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-4, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-3, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-2, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [-1, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [0, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [1, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [2, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [3, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [4, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [5, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [6, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [7, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [8, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] },
+		{ position: [9, 0.5, -6], rotation: [0, 0, 0], scale: [1, 1, 1] }
+	];
+
+	walls = walls.map((wall) => {
+		return {
+			...wall,
+			color: '#F8EBCE',
+			id: uuidv4()
+		}
+	});
+
+	const gameLoop = (timestamp: number) => {
+		const progress = timestamp - lastRender;
+		lastRender = timestamp;
+		moveEnemiesPeriodically(progress);
+		spawnEnemyPeriodically(progress);
+		requestAnimationFrame(gameLoop);
+	};
+
+	const runPeriodicFunction = (fn: { (): void; (): void; }, runEvery: number) => {
+		let counter = 0;
+		return function (deltaTime: number) {
+			counter += deltaTime;
+			if(counter >= runEvery) {
+				fn();
+				counter = 0;
+			}
+		}
+	}
+
+	const moveEnemiesPeriodically = runPeriodicFunction(moveEnemies, 100);
+	const spawnEnemyPeriodically = runPeriodicFunction(spawnEnemy, 1250);
+
+
+	let lastRender = performance.now();
+	requestAnimationFrame(gameLoop)
 </script>
 
-<T.PerspectiveCamera
-  makeDefault
-  position={[-10, 10, 10]}
-  fov={15}
->
-  <OrbitControls
-    autoRotate
-    enableZoom={false}
-    enableDamping
-    autoRotateSpeed={0.5}
-    target.y={1.5}
-  />
+<T.PerspectiveCamera makeDefault position={[-12, 15, 0]} fov={75}>
+	<OrbitControls enableZoom={false} enableDamping autoRotateSpeed={0.5} target.y={1.5} />
 </T.PerspectiveCamera>
 
-<T.DirectionalLight
-  intensity={0.8}
-  position.x={5}
-  position.y={10}
-/>
-<T.AmbientLight intensity={0.2} />
+<T.DirectionalLight intensity={0.8} position.x={5} position.y={10} />
+<T.AmbientLight intensity={1} />
 
-<Grid
-  position.y={-0.001}
-  cellColor="#ffffff"
-  sectionColor="#ffffff"
-  sectionThickness={0}
-  fadeDistance={25}
-  cellSize={2}
-/>
 
-<ContactShadows
-  scale={10}
-  blur={2}
-  far={2.5}
-  opacity={0.5}
-/>
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position.y={1.2}
-    position.z={-0.75}
-  >
-    <T.BoxGeometry />
-    <T.MeshStandardMaterial color="#0059BA" />
-  </T.Mesh>
-</Float>
+<ContactShadows scale={10} blur={2} far={2.5} opacity={0.5} />
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[1.2, 1.5, 0.75]}
-    rotation.x={5}
-    rotation.y={71}
-  >
-    <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-    <T.MeshStandardMaterial color="#F85122" />
-  </T.Mesh>
-</Float>
+{#each walls as wall}
+	<T.Mesh position={[wall.position[0], wall.position[1], wall.position[2]]}>
+		<T.BoxGeometry args={[wall.scale[0], wall.scale[1], wall.scale[2]]} />
+		<T.MeshStandardMaterial color={wall.color} />
+	</T.Mesh>
+{/each}
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
-    <T.IcosahedronGeometry />
-    <T.MeshStandardMaterial color="#F8EBCE" />
-  </T.Mesh>
-</Float>
+<T.Mesh position={[0, 0, 0]}>
+	<T.BoxGeometry args={[20,1,20]} />
+	<T.MeshStandardMaterial color={'#333'} />
+</T.Mesh>
+
+{#each enemies as enemy}
+	<T.Mesh class="enemy" position={[enemy.position[0], enemy.position[1], enemy.position[2]]}>
+		<T.BoxGeometry args={[enemy.scale[0], enemy.scale[1], enemy.scale[2]]} />
+		<T.MeshStandardMaterial color={enemy.color} />
+	</T.Mesh>
+{/each}
